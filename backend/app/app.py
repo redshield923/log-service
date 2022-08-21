835f289cc4d365df4ba15ce5b3f0e574c4470e44
redshield293 <redshield293@gmail.com>
2022-08-21 18:37:23 +0100

    Added authentication

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,28 +1,25 @@
-from datetime import timedelta
-from distutils.spawn import find_executable
+import socket, sqlite3
+from hashlib import sha256
 from typing import Union
-from datetime import datetime
+from datetime import datetime, timedelta
 from jose import JWTError, jwt
-import socket
-from typing import Optional
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import sqlite3
 from models import response
 from models.user import User
+from models.auth import TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 class App:
-    
     app: object
     database_path: str
     oauth2_scheme: object
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     SECRET: str
     ALGORITHM: str
-    
-    def __init__(self, version: str, database_path: str,secret: str, access_token_expires_minutes: int = 60, algorithm: str = 'HS256'):
+    def __init__(self, version: str, database_path: str,secret: str, 
+                 access_token_expires_minutes: int = 60, algorithm: str = 'HS256'):
         self.app: object = FastAPI(version=version)
         self.database_path: str = database_path
         self.oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -38,22 +35,29 @@ class App:
             allow_headers=["*"]
         )
     
-        @self.app.get('/')       
+        @self.app.get('/')     
         def __route_index():
             return self.index()
         
         @self.app.get('/health', response_model=response.Health)
-        def __route_health():
+        def __route_health(current_user: User = Depends(self.get_current_user)):
+            
+            
+            if current_user.username == "benjamin":
+                return HTTPException(status_code=403)
+            
             return self.health()
         
         @self.app.post('/token')
-        async def __route_token(form_data: OAuth2PasswordRequestForm = Depends()):
+        def __route_token(form_data: OAuth2PasswordRequestForm = Depends()):
+            print(form_data)
             return self.login_for_access_token(form_data)
         
         @self.app.get('/user/me')
-        async def __route_get_current_user(current_user: User = Depends(self.get_current_active_user)):
+        def __route_get_current_user(current_user: User = Depends(self.get_current_user)):
+            # current_user = self.get_current_user()
+            # print(current_user)
             return current_user
-    
         
     def get_app(self):
         return self.app
@@ -64,7 +68,7 @@ class App:
     def health(self):
         hostname: str = socket.gethostname()
         res = { "db_health": True, "app_health": True, "hostname": hostname } 
-        
+        database_health = True
         print(f'connecting to database file ${self.database_path}')
         con = sqlite3.connect(self.database_path)
         
@@ -74,7 +78,7 @@ class App:
             cur.execute('''insert into test values (1)''')
             cur.execute('''select * from test''')
             cur.execute('''drop table test''')
-        except Exception:
+        except sqlite3.Error:
             con.commit()
             con.close()
             
@@ -89,35 +93,29 @@ class App:
         return {"token": token}
         
     def get_user(self, username: str):
-        
-        find_user_sql = f"SELECT * FROM user WHERE username = '{username}'"
-        print(find_user_sql)
+        find_user_sql = "SELECT * FROM user WHERE username = ?"
         con = sqlite3.connect(self.database_path)
         con.row_factory = sqlite3.Row  
         cur = con.cursor()
-        cur.execute(find_user_sql)
+        cur.execute(find_user_sql, [username])
         user_response = cur.fetchall()
         user_dict = dict(user_response[0])
         con.close()
-        print(user_dict['username'], type(user_dict['username']))
         
-        if user_response is None: 
+        if user_response is None:
             return None
-        
-        return User(user_dict)
+        return User(**user_dict)
                 
     def authenticate_user(self, username: str, password: str):
-        
         user = self.get_user(username)
         if not user:
             return False
-        if not user.correct_password(password=password):
+        if not self.correct_password(user.user_password, password):
             return False
-        return user 
+        return user
     
     def login_for_access_token(self, form_data: OAuth2PasswordRequestForm = Depends()):
-        user = self.authenticate_user(form_data.username, form_data.password)
-        
+        user: User = self.authenticate_user(form_data.username, form_data.password)
         if not user: 
             raise HTTPException(status_code=401, detail="Incorrect Username or Password")
         
@@ -125,6 +123,7 @@ class App:
         access_token = self.create_access_token(
             data={"sub": user.username}, expires_delta=access_token_expires
         )
+        
         return {"access_token": access_token, "token_type": "bearer"}
     
     def create_access_token(self, data: dict, expires_delta: Union[timedelta, None] = None):
@@ -137,15 +136,16 @@ class App:
         encoded_jwt = jwt.encode(to_encode, self.SECRET, algorithm=self.ALGORITHM)
         return encoded_jwt
     
-    async def get_current_user(self, token: str = Depends(oauth2_scheme)):
+    def get_current_user(self, token: str = Depends(oauth2_scheme)):
         credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
         try:
             payload = jwt.decode(token, self.SECRET, algorithms=[self.ALGORITHM])
             username: str = payload.get("sub")
+            
             if username is None:
                 raise credentials_exception
             token_data = TokenData(username=username)
@@ -156,9 +156,8 @@ class App:
             raise credentials_exception
         return user
     
-    async def get_current_active_user(current_user: User = Depends(get_current_user)):
-        if not current_user.active:
-            raise HTTPException(status_code=400, detail="Inactive user")
-        return current_user
+    def correct_password(self, password_hash: str, password: str):
+
+        return True if password_hash == sha256(password.encode('utf-8')).hexdigest() else False
 
 
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,34 +1,17 @@
 from datetime import datetime
 from hashlib import sha256
+from pickletools import string1
+from pydantic import BaseModel
 
-class User():
-    user_id: int
+class User(BaseModel):
     username: str
+    id: int
     active: int
-    password_hash: str
-    active: int
+    user_password: str
     time_created: datetime
     time_updated: datetime
     updated_by: str
-    
-    def __init__(self, user: object):
-            self.username = user['username'] 
-            self.user_id = user['id']
-            self.password_hash = user['user_password']
-            self.active = user['active']
-            self.time_created = user['time_created']
-            self.time_updated = user['time_updated']
-            self.updated_by = user['updated_by']
-            
-    def correct_password(self, password: str):
-        
-        given_password_hash = sha256(password.encode('utf-8')).hexdigest()
-        
-        if self.password_hash == given_password_hash:
-            return True
-        
-        return True if self.password_hash == given_password_hash else False
-        
-        
+    type: int
+
     
     

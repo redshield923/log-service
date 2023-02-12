354945455d1f0213bd49620df5fdb8665af88b2c
redshield293 <redshield293@gmail.com>
2023-02-12 13:25:44 +0000

    🐞 fix(backend): ammended SQl scripts with new hash

--- app/database/scripts/insert_test_data.sql
+++ app/database/scripts/insert_test_data.sql
@@ -3,5 +3,5 @@
 INSERT INTO ref_usertype (type) VALUES ('user')
 INSERT INTO ref_usertype (type) VALUES ('admin')
 INSERT INTO ref_usertype (type) VALUES ('superadmin')
-INSERT INTO user (id, username, password, active, time_created, time_updated, updated_by, type) VALUES (1,'testadmin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 1, julianday('now'), julianday('now'), 1, 2)
-INSERT INTO user (id, username, password, active, time_created, time_updated, updated_by, type) VALUES (2,'testuser', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 1, julianday('now'), julianday('now'), 1, 1)
+INSERT INTO user (id, username, password, active, time_created, time_updated, updated_by, type) VALUES (1,'testadmin', '$argon2id$v=19$m=65536,t=3,p=4$lGQxm45YoJiMzopOT61bkA$tYVNFK/TJ0Mm7qFEeSAnGXCEM9Q7gRl/yE+pfN9JoQk', 1, julianday('now'), julianday('now'), 1, 2)
+INSERT INTO user (id, username, password, active, time_created, time_updated, updated_by, type) VALUES (2,'testuser', '$argon2id$v=19$m=65536,t=3,p=4$lGQxm45YoJiMzopOT61bkA$tYVNFK/TJ0Mm7qFEeSAnGXCEM9Q7gRl/yE+pfN9JoQk', 1, julianday('now'), julianday('now'), 1, 1)

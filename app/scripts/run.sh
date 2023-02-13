974b28cd9c84aea8b82781a4e681f692a8736f7e
redshield293 <redshield293@gmail.com>
2023-02-13 16:13:45 +0000

    ✨ feat(backend): application now runs in HTTPS

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -14,27 +14,14 @@ Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 BRed='\033[1;31m'         # Red
 
-echo -e "\n${Blue}Starting local build of ${Purple}log-service ${Blue}...${Reset}"
-python3 -m pip install --no-cache-dir --upgrade -r requirements.txt 
-
-if [ $? -nq 0 ]; then
-    echo "${Red}Pip install failed. Do you have Python3 installed?${Reset}"
-    exit 1
-fi
-
-echo -e "\n${Green}Finished local build of ${Blue}log-service${Green}.${Reset}"
-echo -e "\n${Blue}Running tests...${Reset}"
-source ./test.sh
-echo -e "\n${Green}Tests finished. See above for results.${Reset}"
-
 echo -e "\n${Blue}Starting docker build of ${Purple}log-service${Blue}...${Reset}"
 docker build -f Dockerfile -t log-service:0.0.1 .
 docker tag log-service:0.0.1 log-service:latest 
 docker run --rm -d --name log-service -p 80:80 log-service:latest
 
-if [ $? -nq 0 ]; then
+if [ $? -ne 0 ]; then
     echo "${Red}Failed to build or run Docker container. Is docker installed and running?${Reset}"
     exit 1
 fi
 
-echo -e "\n${Blue}log-service${Green} running! Check out ${BRed}http://0.0.0.0/docs ${Green} to get started.${Reset}"
+echo -e "\n${Blue}log-service${Green} running! Check out ${BRed}https://0.0.0.0:80/docs ${Green} to get started.${Reset}"
